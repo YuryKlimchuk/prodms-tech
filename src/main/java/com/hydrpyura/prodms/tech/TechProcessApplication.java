35582040cd06d1ee6f1b7f628e7df578f6fb4e2d
yury.klimchuk <hydro.yura@gmail.com>
2023-09-24 18:07:40 +0300

    init commit

---
+++ src/main/java/com/hydrpyura/prodms/tech/TechProcessApplication.java
@@ -0,0 +1,13 @@
+package com.hydrpyura.prodms.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechProcessApplication.class, args);
+	}
+
+}
